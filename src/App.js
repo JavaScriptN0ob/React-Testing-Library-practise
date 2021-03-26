4b90b4861c5eb5c07a40015f2fa10bc96323ccd7
JavaScriptN0ob <wenpeijs@gmail.com>
2021-03-26 15:11:30 +1100

    test on checkbox and button disabled attribute

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-// import logo from './logo.svg';
+// import logo from
 import { useState } from 'react';
 import './App.css';
 
 function App() {
   const [ buttonColor, setButtonColor ] = useState('red');
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  const [checkboxStatus, setCheckboxStatus] = useState(false);
 
   return (
-    <div>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      height: '30vh',
+      alignItems: 'center',
+    }}>
       <button
-        style={{backgroundColor: buttonColor}}
+        style={{
+          backgroundColor: buttonColor,
+        }}
         onClick={() => {
           setButtonColor(newButtonColor)
         }}
+        disabled={checkboxStatus}
       >
         Change to {newButtonColor}
       </button>
+      <input
+        type="checkbox"
+        checked={checkboxStatus}
+        onChange={(e) => setCheckboxStatus(e.target.checked)}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -19,6 +19,28 @@ test('button turns blue when clicked', () => {
   fireEvent.click(colorButton);
 
   expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
-
   expect(colorButton.textContent).toBe('Change to red');
+});
+
+test('initial conditions', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+});
+
+test('button disabled when checkbox is checked', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const checkbox = screen.getByRole('checkbox');
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
 });
